a9c3f111fa046c5c8541db4c6e379fa89817ab8a
cc <62285377+chenchouchou888@users.noreply.github.com>
2022-06-03 16:23:18 +0800

    e

---
+++ src/components/news/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'choerodon-ui';
+
+const index:React.FC = () => {
+  return (
+    <div className="App">
+        <Button color='primary'>Button</Button>
+    </div>
+  );
+}
+
+export default index;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "choerodon-ui/lib/configure"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
